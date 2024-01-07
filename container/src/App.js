c9460638def87941ab3b51d284a917cd403ff3db
Dawid Femin <dawid.femin@gmail.com>
2024-01-07 21:22:40 +0100

    Added Header Component

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Marketing from "../components/Marketing";
+import Header from "../components/Header";
+import {BrowserRouter} from "react-router-dom";
 
 export default () => {
     return (
-        <>
-            <h1>Hi there!</h1>
-            <hr/>
+        <BrowserRouter>
+            <div>
+                <Header/>
+            </div>
             <Marketing/>
-        </>
+        </BrowserRouter>
     )
 };
