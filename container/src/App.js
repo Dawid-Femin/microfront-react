84cfd21968f00c8544fea241a128b743c13afdef
Dawid Femin <dawid.femin@gmail.com>
2024-01-07 22:05:30 +0100

    Randomize production className

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Marketing from "../components/Marketing";
 import Header from "../components/Header";
 import {BrowserRouter} from "react-router-dom";
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header/>
-            </div>
-            <Marketing/>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header/>
+                </div>
+                <Marketing/>
+            </StylesProvider>
         </BrowserRouter>
     )
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -4,11 +4,15 @@ import {StylesProvider} from "@material-ui/core/styles";
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+import {createGenerateClassName} from "@material-ui/core";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+})
 export default () => {
     return (
         <>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path='/pricing' component={Pricing}/>
