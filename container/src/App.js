b4d5e1dcfea7f112d9759998d3fc503abcfdf78f
Dawid Femin <dawid.femin@gmail.com>
2024-01-08 01:58:23 +0100

    Added loading progress bar

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,7 @@
 import React, {lazy, Suspense} from 'react';
 import Header from "../components/Header";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import {createGenerateClassName, StylesProvider} from "@material-ui/core";
+import {LinearProgress, createGenerateClassName, StylesProvider} from "@material-ui/core";
 
 const MarketingLazy = lazy(() => import('../components/Marketing'));
 const AuthLazy = lazy(() => import('../components/Auth'));
@@ -15,7 +15,7 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header/>
-                    <Suspense fallback={<div>Loading...</div>}>
+                    <Suspense fallback={<LinearProgress />}>
                         <Switch>
                             <Route path='/auth' component={AuthLazy}/>
                             <Route path='/' component={MarketingLazy}/>
