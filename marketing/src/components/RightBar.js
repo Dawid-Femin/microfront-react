5646bdd3bb1e5ef53a6f23db1909df2f59a64d12
Dawid Femin <dawid.femin@benefitsystems.pl>
2024-01-11 16:56:23 +0100

    Created RightBar component

---
+++ marketing/src/components/RightBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@mui/material";
+
+const RightBar = () => {
+    return (
+        <>
+            <Box flex={2} p={2} sx={{display: {xs: 'none', lg: 'block'}}}>RIGHT BAR</Box>
+        </>
+    )
+}
+
+export default RightBar;
